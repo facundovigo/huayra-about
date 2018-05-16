288a42d77956b2fd6a51c009868cab685669fb36
Diego Accorinti <diegoacco@gmail.com>
2018-05-16 18:35:28 -0300

    setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from distutils.core import setup
 
 setup(name = "huayra-about",
-      version = "0.1.1",
+      version = "0.2.6",
       description = "Acerca de Huayra",
       author = "Pedro Boliche",
       author_email = "bolichep@gmail.com",
-      url = "https://github.com/bolichep/huayra-about",
+      url = "https://github.com/HuayraLinux/huayra-about",
 #      py_modules = ["huayra-about"],
 #      scripts = ["huayra-about"],
       data_files = [ ("share/huayra-about", ["src/huayra-about.py","src/rasti.py","src/markup.py","src/info_table.py"]),
-                     ("share/huayra-about/media", ["src/media/huayra-menu-huayra.svg","src/media/huayra-about-background.svg"]),
-                     ("share/huayra-about/plugins", ["src/plugins/__init__.py","src/plugins/01versiones.py","src/plugins/02arch.py","src/plugins/03cpu_mem.py","src/plugins/04kernel.py","src/plugins/05link.py"]),
+                     ("share/huayra-about/media", ["src/media/huayra-menu-huayra.svg","src/media/huayra-about-background.svg","src/media/compartir.svg"])
+                     ("share/huayra-about/plugins", ["src/plugins/__init__.py","src/plugins/01versiones.py","src/plugins/02arch.py","src/plugins/03cpu_mem.py","src/plugins/04kernel.py","src/plugins/05link.py","src/plugins/06hdparticiones.py"]),
                      ("share/applications", ["huayra-about.desktop"]),
                      ("share/icons/hicolor/scalable/apps",["src/media/huayra-about.svg"]),
                      ("share/man/man1", ["huayra-about.1"]) ] )
