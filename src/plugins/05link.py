bf7fcd5841aba00fd5cb60734258a645e8e2366b
Diego Accorinti <diegoacco@gmail.com>
2018-05-16 18:03:56 -0300

    fix URL

--- src/plugins/05link.py
+++ src/plugins/05link.py
@@ -3,7 +3,7 @@ import info_table
 
 # Link
 web_label = markup.label_start_markup + "info" + markup.label_end_markup
-web_link = markup.text_start_markup + "<a href='http://huayra-conectarigualdad.gob.ar/'>sitio web</a>" + markup.text_end_markup
+web_link = markup.text_start_markup + "<a href='http://huayra.conectarigualdad.gob.ar/'>sitio web</a>" + markup.text_end_markup
 
 
 info_table.add_row_to_table(web_label, web_link, 6, destino="gui")
